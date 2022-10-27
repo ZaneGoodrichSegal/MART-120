1648892976dbacef3b2bfa79f7148e1863298782
Zane! <112509824+ZaneGoodrichSegal@users.noreply.github.com>
2022-10-26 20:52:54 -0600

    Add files via upload

---
+++ homework Week 9/JS/sketch.js
@@ -0,0 +1,81 @@
+function setup() {
+  createCanvas(400,400);
+}
+
+function draw() {
+  background(42, 201, 122);
+  
+  stroke(6)
+  fill(102,51,0)
+  circle(200,10,60)
+  circle(240,20,60)
+  circle(260,30,60)
+  circle(280,50,60)
+  circle(300,70,60)
+  circle(305,90,60)
+  circle(190,10,60)
+  circle(175,20,60)
+  circle(150,30,60)
+  circle(140,50,60)
+  circle(125,70,60)
+  circle(1110,90,60)
+  
+  
+  fill(218,112,214)
+  ellipse(200,400,600,300)
+  fill(95,158,160)
+  stroke(0)
+  strokeWeight(5)
+  triangle(50,300,400,400,300,300)
+
+  fill (225, 201, 122)
+  strokeWeight(3)
+  ellipse(100,150,30,50)
+  ellipse(300,150,30,50)
+  strokeWeight(4)
+  ellipse(200,175,200,325)
+  
+  
+  strokeWeight(2)
+  stroke(0)
+  fill(127,0,0)
+  circle(145,125,75)
+  fill(100,100,100)
+  ellipse(145,125,75,45)
+  fill(255,255,255)
+  circle(145,125,45)
+  fill(220,20,60)
+  circle(145,125,20)
+  
+  strokeWeight(2)
+  stroke (0)
+  fill(127,0,0)
+  circle(250,125,75)
+  fill(100,100,100)
+  ellipse(250,125,75,45)
+  fill(255,255,255)
+  circle(250,125,45)
+  fill(220,20,60)
+  circle(250,125,20)
+  
+  
+  strokeWeight(10)
+  stroke(45,140)
+  point(265,250)
+  point(255,235)
+  point(275,265)
+  
+  stroke(0)
+  strokeWeight(5)
+  line(150,250,250,250)
+  strokeWeight(3)
+  fill(153,76,0)
+  rect(160,200,75,30)
+  
+  fill(255,255,255)
+  strokeWeight(4)
+  textFont('Courier New italic',10)
+  text('Zane "bigdog" Segal', 270,375)
+  
+  
+}
