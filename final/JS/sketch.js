7ec26d524f348164dd1821dba5eef5d448860b35
Zane! <112509824+ZaneGoodrichSegal@users.noreply.github.com>
2022-12-16 16:58:17 -0700

    Add files via upload

---
+++ final/JS/sketch.js
@@ -0,0 +1,53 @@
+function setup() {
+  createCanvas(400, 400);
+}
+
+function draw() {
+  background(97,86,80);
+  
+  fill(35,51,40)
+  triangle(300,200,200,350,400,350)
+  fill(60,86,13)
+  rect(250,125,100,200)
+  
+  fill(211,168,151)
+  ellipse(295,95,45,60)
+  
+  fill(210,209,204)
+  triangle(270,80,295,60,325,80)
+  triangle(315,80,310,120,330,120)
+  
+  fill(61,105,150)
+  rect(295,175,30,15)
+  rect(220,160,30,15)
+  
+  fill(207,176,155)
+  ellipse(280,180,40,20)
+  ellipse(205,165,40,20)
+  
+  
+  fill(15,17,30)
+  triangle(90,110,155,200,90,200)
+  fill(28,29,47)
+  triangle(90,110,50,210,90,200)
+  fill(55,39,52)
+  triangle(90,90,50,300,150,300)
+  
+  fill(197,166,148)
+  ellipse(90,90,40,50)
+  fill(19,18,26)
+  ellipse(90,70,50,20)
+  rect(75,35,30,40)
+  
+  fill(190,152,139)
+  ellipse(160,190,40,20)
+  ellipse(50,190,20,40)
+  
+  fill(25,33,52)
+  rect(75,300,15,40)
+  rect(105,300,15,40)
+  
+  textSize(15)
+  textFont('Georgia');
+  text('The Arnolfimi Portrait by Jan Van Eyck', 100,25)
+}
